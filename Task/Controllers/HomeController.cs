0a1748272baa21203c4c2070ced5348df37212d5
Maqa <Maqa@DESKTOP-8B9UD5J>
2022-03-15 19:14:13 -0700

    Add project files.

---
+++ Task/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Task.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public ContentResult Index()
+        //{
+        //    ContentResult contentResult = new ContentResult();
+        //    contentResult.Content = "P225 Content";
+        //    contentResult.ContentType = "Get Index";
+        //    contentResult.StatusCode = (int?)HttpStatusCode.OK;
+
+        //    return contentResult;
+
+        //}
+
+        //public ViewResult Index()
+        //{
+        //    ViewResult viewResult = new ViewResult();
+        //    viewResult.ViewName = "test";
+        //    return viewResult;
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
